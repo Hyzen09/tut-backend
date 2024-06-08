c6de6afb196c1581beeed172df7c779aa5d6bd60
Hyzen09 <vedang1709@gmail.com>
2024-06-09 00:56:39 +0530

    add register controller

--- src/controllers/user.controler.js
+++ src/controllers/user.controler.js
@@ -1,10 +1,71 @@
 import router from "../routes/user.routes.js";
 import { asynchandeler } from "../utils/asyncHandeler.js";
-
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uplodeOnCloudinary } from "../utils/coludinary.js"
+import { ApiResponce } from "../utils/ApiResponce.js";
 const registerUser = asynchandeler(async (req, res) => {
-    res.status(200).json({
-        message: 'OK'
+    // get user details from frontend.
+    //validation -not empty
+    //check if user already exist : through sername and email
+    //check for images , check for avatar
+    // upload them to cloudinary , avatar check
+    //creat user object - create entry im db
+    //remove password and refresh token field from responce
+    //check for user creation 
+    // return responce
+
+    const { username, email, fullname, password } = req.body
+    console.log("email", email);
+
+    if (
+        [username, email, fullname, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "all fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     })
+    if (existedUser) {
+        throw ApiError(409, "this account is already register")
+    }
+
+    const avatarLocalPath = req.filse?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "avatar files is required")
+    }
+
+    const avatar = await uplodeOnCloudinary(avatarLocalPath)
+    const coverImage = await uplodeOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "avatar files is required")
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser  = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500 , "something went wrong while registering a user")
+        
+    }
+
+    return res.status(201).json(
+        new ApiResponce(200, createdUser,"user register sucessfully")
+    )
 })
 
-export { registerUser }   
+export { registerUser }    
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema({
     },
     watchHistory:[
         {
-            type:Schema.Types,ObjectID,
+            type:Schema.Types.ObjectID,
             ref:"Video"
         }
     ],
@@ -55,7 +55,7 @@ userSchema.pre("save",async function (next){
     {
         return next();
     }
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
